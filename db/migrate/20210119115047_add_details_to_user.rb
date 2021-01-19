87b916b917b36e976ff251cf973c2834b89b0c53
Chafik Argoub <cargoub@gmail.com>
2021-01-19 12:52:10 +0100

    add details to user

---
+++ db/migrate/20210119115047_add_details_to_user.rb
@@ -0,0 +1,9 @@
+class AddDetailsToUser < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :company_name, :string
+    add_column :users, :phone_number, :string
+    add_column :users, :description, :text
+  end
+end
