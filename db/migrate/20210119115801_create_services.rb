80894d946188ea4fbe88d5e7889a527efcf96898
Chafik Argoub <cargoub@gmail.com>
2021-01-19 12:59:01 +0100

    add service model

---
+++ db/migrate/20210119115801_create_services.rb
@@ -0,0 +1,12 @@
+class CreateServices < ActiveRecord::Migration[6.0]
+  def change
+    create_table :services do |t|
+      t.string :name
+      t.integer :price_per_day
+      t.text :description
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
